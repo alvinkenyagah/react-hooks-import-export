92bc4406de2f109632288bf35e65d05e5207941f
alvinkenyagah <alvin47k@gmail.com>
2023-04-18 15:46:17 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife,elevation} from "./parks/RockyMountain.js";
 
-function ColoradoStateParks() {
+
+
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  
+  return (
+  <>
+  <h1>Colorado State Parks!</h1>
+  {MesaVerde()}
+  {trees}
+  {wildlife()}
+  {elevation()}
 
-  return <h1>Colorado State Parks!</h1>;
+  </>
+  
+  )
+  
 }
+
